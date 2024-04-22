a19aaa7dc39bde467d1daf6e3a6c85442f584f19
samuelblanco <dev.sam23d@gmail.com>
2024-04-22 14:03:45 -0700

    Adds dependencies

--- mix.exs
+++ mix.exs
@@ -57,7 +57,9 @@ defmodule SleeperPuzzle.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:libcluster, "~> 3.3"},
+      {:swarm, "~> 3.0"}
     ]
   end
 
